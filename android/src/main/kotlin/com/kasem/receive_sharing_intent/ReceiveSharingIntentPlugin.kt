fb73e6032c5baec77fc4b78f33df5a4da0d25df9
Leonard Günther <leonard@eynck.de>
2026-01-19 13:29:53 +0100

    Update Gradle and Kotlin versions + Fix dropped intent issue (#1)
    * Update Gradle Plugin version
    
    * Fix dropped intent issues
    
    * Remove local.properties usage
    
    * Update Kotlin version
    
    * Move FileDirectory to same file
    
    because when using the package the reference is not found
    
    * Don't set intent to null

--- android/src/main/kotlin/com/kasem/receive_sharing_intent/ReceiveSharingIntentPlugin.kt
+++ android/src/main/kotlin/com/kasem/receive_sharing_intent/ReceiveSharingIntentPlugin.kt
@@ -1,7 +1,9 @@
 package com.kasem.receive_sharing_intent
 
+import android.content.ContentUris
 import android.content.Context
 import android.content.Intent
+import android.database.Cursor
 import android.graphics.Bitmap
 import android.media.MediaMetadataRetriever
 import android.media.MediaMetadataRetriever.METADATA_KEY_DURATION
@@ -9,6 +11,11 @@ import android.media.MediaMetadataRetriever.OPTION_CLOSEST_SYNC
 import android.net.Uri
 import android.os.Parcelable
 import android.os.Build
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.util.Log
+import android.webkit.MimeTypeMap
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -24,6 +31,7 @@ import org.json.JSONObject
 import java.io.File
 import java.io.FileOutputStream
 import java.net.URLConnection
+import java.util.Date
 
 private const val MESSAGES_CHANNEL = "receive_sharing_intent/messages"
 private const val EVENTS_CHANNEL_MEDIA = "receive_sharing_intent/events-media"
@@ -69,7 +77,7 @@ class ReceiveSharingIntentPlugin : FlutterPlugin, ActivityAware, MethodCallHandl
 
     override fun onMethodCall(call: MethodCall, result: Result) {
         when (call.method) {
-            "getInitialMedia" -> result.success(initialMedia?.toString())
+            "getInitialMedia" -> result.success((latestMedia ?: initialMedia)?.toString())
             "reset" -> {
                 initialMedia = null
                 latestMedia = null
@@ -90,7 +98,7 @@ class ReceiveSharingIntentPlugin : FlutterPlugin, ActivityAware, MethodCallHandl
 
                 val value = getMediaUris(intent)
                 if (initial) initialMedia = value
-                latestMedia = value
+                if (!initial) latestMedia = value
                 eventSinkMedia?.success(latestMedia?.toString())
             }
 
@@ -102,7 +110,7 @@ class ReceiveSharingIntentPlugin : FlutterPlugin, ActivityAware, MethodCallHandl
                                 .put("type", MediaType.URL.value))
                 )
                 if (initial) initialMedia = value
-                latestMedia = value
+                if (!initial) latestMedia = value
                 eventSinkMedia?.success(latestMedia?.toString())
             }
         }
@@ -186,7 +194,13 @@ class ReceiveSharingIntentPlugin : FlutterPlugin, ActivityAware, MethodCallHandl
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
         this.binding = binding
         binding.addOnNewIntentListener(this)
-        handleIntent(binding.activity.intent, true)
+        val intent = binding.activity.intent
+        // We set the activity intent to null after handling it to avoid that the os
+        // calls onAttachedToActivity again with the same intent
+        binding.activity.intent = Intent()
+        if (intent.action != null) {
+            handleIntent(intent, true)
+        }
     }
 
     override fun onDetachedFromActivityForConfigChanges() {
@@ -215,5 +229,161 @@ class ReceiveSharingIntentPlugin : FlutterPlugin, ActivityAware, MethodCallHandl
     inline fun <reified T : Parcelable> Intent.parcelableArrayList(key: String): ArrayList<T>? = when {
         Build.VERSION.SDK_INT >= 33 -> getParcelableArrayListExtra(key, T::class.java)
         else -> @Suppress("DEPRECATION") getParcelableArrayListExtra(key)
+    }
+}
+
+
+object FileDirectory {
+
+    /**
+     * Get a file path from a Uri. This will get the the path for Storage Access
+     * Framework Documents, as well as the _data field for the MediaStore and
+     * other file-based ContentProviders.
+     *
+     * @param context The context.
+     * @param uri The Uri to query.
+     * @author paulburke
+     */
+    fun getAbsolutePath(context: Context, uri: Uri): String? {
+
+        // DocumentProvider
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                val type = split[0]
+
+                return if ("primary".equals(type, ignoreCase = true)) {
+                    Environment.getExternalStorageDirectory().toString() + "/" + split[1]
+                } else {
+                    getDataColumn(context, uri, null, null)
+                }
+            } else if (isDownloadsDocument(uri)) {
+                return try {
+                    val id = DocumentsContract.getDocumentId(uri)
+                    val contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(id))
+
+                    getDataColumn(context, contentUri, null, null)
+                } catch (exception: Exception) {
+                    getDataColumn(context, uri, null, null)
+                }
+            } else if (isMediaDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                val type = split[0]
+
+                var contentUri: Uri? = null
+                when (type) {
+                    "image" -> contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    "video" -> contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                    "audio" -> contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                }
+
+                if (contentUri == null) return null
+
+                val selection = "_id=?"
+                val selectionArgs = arrayOf(split[1])
+                return getDataColumn(context, contentUri, selection, selectionArgs)
+            }// MediaProvider
+            // DownloadsProvider
+        } else if ("content".equals(uri.scheme, ignoreCase = true)) {
+            return getDataColumn(context, uri, null, null)
+        }
+
+        return uri.path
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context The context.
+     * @param uri The Uri to query.
+     * @param selection (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    private fun getDataColumn(context: Context, uri: Uri, selection: String?,
+                              selectionArgs: Array<String>?): String? {
+
+        if (uri.authority != null) {
+            var cursor: Cursor? = null
+            val column = "_display_name"
+            val projection = arrayOf(column)
+            var targetFile: File? = null
+            try {
+                cursor = context.contentResolver.query(uri, projection, selection, selectionArgs, null)
+                if (cursor != null && cursor.moveToFirst()) {
+                    val columnIndex = cursor.getColumnIndexOrThrow(column)
+                    val fileName = cursor.getString(columnIndex)
+                    Log.i("FileDirectory", "File name: $fileName")
+                    targetFile = File(context.cacheDir, fileName)
+                }
+            } finally {
+                cursor?.close()
+            }
+
+            if (targetFile == null) {
+                val mimeType = context.contentResolver.getType(uri)
+                val prefix = with(mimeType ?: "") {
+                    when {
+                        startsWith("image") -> "IMG"
+                        startsWith("video") -> "VID"
+                        else -> "FILE"
+                    }
+                }
+                val type = MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType)
+                targetFile = File(context.cacheDir, "${prefix}_${Date().time}.$type")
+            }
+
+            context.contentResolver.openInputStream(uri)?.use { input ->
+                FileOutputStream(targetFile).use { fileOut ->
+                    input.copyTo(fileOut)
+                }
+            }
+            return targetFile.path
+        }
+
+        var cursor: Cursor? = null
+        val column = "_data"
+        val projection = arrayOf(column)
+
+        try {
+            cursor = context.contentResolver.query(uri, projection, selection, selectionArgs, null)
+            if (cursor != null && cursor.moveToFirst()) {
+                val columnIndex = cursor.getColumnIndexOrThrow(column)
+                return cursor.getString(columnIndex)
+            }
+        } finally {
+            cursor?.close()
+        }
+        return null
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    fun isExternalStorageDocument(uri: Uri): Boolean {
+        return "com.android.externalstorage.documents" == uri.authority
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    fun isDownloadsDocument(uri: Uri): Boolean {
+        return "com.android.providers.downloads.documents" == uri.authority
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    fun isMediaDocument(uri: Uri): Boolean {
+        return "com.android.providers.media.documents" == uri.authority
     }
 }
